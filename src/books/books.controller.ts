dcba4cbb98c0a3af1b466879f818b350ffb839c6
siddigamon <115434776+siddigamon@users.noreply.github.com>
2025-07-25 13:50:08 +0800

    feat: implement CRUD operations for books and add author filtering

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Post()
+  create(@Body() createBookDto: CreateBookDto) {
+    return this.booksService.create(createBookDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.booksService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.booksService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateBookDto: UpdateBookDto,
+  ) {
+    return this.booksService.update(id, updateBookDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.booksService.remove(id);
+  }
+  @Get('author/:authorId')
+  findByAuthor(@Param('authorId', ParseIntPipe) authorId: number) {
+    return this.booksService.findByAuthor(authorId);
+  }
+}
