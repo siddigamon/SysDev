9cc278129acc27c4c6802fede8492ca31f0af7be
siddigamon <115434776+siddigamon@users.noreply.github.com>
2025-07-25 12:39:30 +0800

    feat: added crud operations for authors

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { AuthorsService } from './authors.service';
+import { CreateAuthorDto } from './dto/create-author.dto';
+import { UpdateAuthorDto } from './dto/update-author.dto';
 
 @Controller('authors')
-export class AuthorsController {}
+export class AuthorsController {
+  constructor(private readonly authorsService: AuthorsService) {}
+
+  @Post()
+  create(@Body() createAuthorDto: CreateAuthorDto) {
+    return this.authorsService.create(createAuthorDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.authorsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.authorsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateAuthorDto: UpdateAuthorDto,
+  ) {
+    return this.authorsService.update(id, updateAuthorDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.authorsService.remove(id);
+  }
+}
--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateAuthorDto } from './dto/create-author.dto';
+import { UpdateAuthorDto } from './dto/update-author.dto';
 @Injectable()
-export class AuthorsService {}
+export class AuthorsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createAuthorDto: CreateAuthorDto) {
+    return this.prisma.author.create({
+      data: createAuthorDto,
+    });
+  }
+
+  async findAll() {
+    return this.prisma.author.findMany({
+      include: { books: true },
+    });
+  }
+
+  async findOne(id: number) {
+    const author = await this.prisma.author.findUnique({
+      where: { id },
+      include: { books: true },
+    });
+
+    if (!author) {
+      throw new NotFoundException(`Author with ID ${id} not found`);
+    }
+
+    return author;
+  }
+
+  async update(id: number, updateAuthorDto: UpdateAuthorDto) {
+    await this.findOne(id);
+
+    return this.prisma.author.update({
+      where: { id },
+      data: updateAuthorDto,
+      include: { books: true },
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    return this.prisma.author.delete({
+      where: { id },
+    });
+  }
+}
